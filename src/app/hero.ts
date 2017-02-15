075fabcc47e1561053d59b049023b7d0b2067dbe
jinsil <h.siri1205@gmail.com>
2017-02-15 16:21:14 +0900

    second

---
+++ src/app/hero.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by siri on 2017-02-13.
+ */
+export class Hero {
+  constructor(
+    public id: number,
+    public name: string) { }
+}
+
+/*
+export class Hero {
+
+ id: number;
+name: string;
+}
+*/
